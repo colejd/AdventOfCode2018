1a1fec32a7d49f973bdca60f01c8cbb6a86e8838
Jonathan Cole <joncole04412@gmail.com>
2018-12-10 11:42:16 -0500

    Add rate limiting decorator

--- aoc_helpers/perf_helpers.py
+++ aoc_helpers/perf_helpers.py
@@ -12,6 +12,22 @@ def timeit(method):
             name = kw.get('log_name', method.__name__.upper())
             kw['log_time'][name] = int((te - ts) * 1000)
         else:
-            print('%r took %2.2f ms' % (method.__name__, (te - ts) * 1000))
+            print('=======================\n%r took %2.2f ms' % (method.__name__, (te - ts) * 1000))
         return result
     return timed
+
+
+def timer(t):
+    """
+    Function decorator that enforces a time interval between function calls.
+    """
+    def wrapper(f):
+        @wraps(f)
+        def wrapped_func(*args, **kwargs):
+            now = time.time()
+            if now - wrapped_func.latest > t:
+                f(*args, **kwargs)
+                wrapped_func.latest = now
+        wrapped_func.latest = time.time()
+        return wrapped_func
+    return wrapper
